a39845602cee747e229e68d6fdee1c737a29b06c
tkddn5623 <tkddn5623@g.skku.edu>
2023-10-01 21:54:44 +0900

    java example 5

---
+++ javaProgrammingLab5/BasePlusCommissionEmployee.java
@@ -0,0 +1,45 @@
+public class BasePlusCommissionEmployee
+extends CommissionEmployee {
+	private double baseSalary; // base salary per week
+
+	// six-argument constructor
+	public BasePlusCommissionEmployee(String first, String last, String ssn,
+			double sales, double rate, double salary) {
+		super(first, last, ssn, sales, rate);
+		setBaseSalary(salary); // validate and store base salary
+	} // end six-argument BasePlusCommissionEmployee constructor
+
+	// set base salary
+	public void setBaseSalary(double salary) {
+		if (salary >= 0.0)
+			baseSalary = salary;
+		else
+			throw new IllegalArgumentException("Base salary must be >= 0.0");
+	} // end method setBaseSalary
+
+	// return base salary
+	public double getBaseSalary() {
+		return baseSalary;
+	} // end method getBaseSalary
+
+	// calculate earnings
+	@Override
+	// indicates that this method overrides a superclass method
+	public double earnings() {
+		return baseSalary + ( commissionRate * grossSales );
+
+	} // end method earnings
+
+	// return String representation of BasePlusCommissionEmployee
+	@Override
+	// indicates that this method overrides a superclass method
+	public String toString() {
+		return String.format("%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n%s: %.2f",
+				"base-salaried commission employee", getFirstName(),
+				lastName, "social security number",
+				getSocialSecurityNumber(), "gross sales", getGrossSales(),
+				"commission rate", getCommissionRate(), "base salary",
+				baseSalary);
+	} // end method toString
+} // end class BasePlusCommissionEmployee
+
