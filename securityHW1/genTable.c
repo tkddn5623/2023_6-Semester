c5fad286c993fea616b2ebcf0c83bd96fdc1d3b5
tkddn5623 <tkddn5623@g.skku.edu>
2023-10-07 22:36:11 +0900

    AES almost done...

--- securityHW1/genTable.c
+++ securityHW1/genTable.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include "aes.h"
-#define PLAINTEXT (0x00000000000000000000000000000000)
-#define BUFSIZE (10000)
+#include <stdio.h>  ////////////////////////////////////////////
+#include <stdlib.h> /// Date:   2023-10-17                   ///
+#include <string.h> /// Author: jung sang woo                ///
+#include <stdint.h> ///                                      ///
+#include "aes.h"    /// This is third party AES header       ///
+////////////////////////////////////////////
+#define REDUCTCHAIN (3)
+#define BUFSIZE     (10000)
 
 static char buffer[BUFSIZE];
+static int PWBITS;
 
 int argv_valid_check(const char* s) {
 	int a;
@@ -24,7 +26,7 @@ int str_to_int(const char* s) {
 char* aes_to_str(char buffer[], uint8_t key[], int length) {
 	uint8_t k;
 	for (int i = 0; i < length; i++) {
-		
+
 	}
 	return NULL;
 }
@@ -42,32 +44,72 @@ int integerSqrt(int N) {
 	}
 	return root;
 }
-void reduction() {
+uint8_t* uint8v_add1(uint8_t u8vec[], int length) {
+	while (length-- > 0) {
+		if ((u8vec[length] += 1) == 0) break;
+	}
+	return u8vec;
+}
+void reduction(uint8_t dest[], const uint8_t src[], int destlen, int srclen) {
+	int quotient = PWBITS / 8;
+	int reminder = PWBITS % 8;
+	memset(dest, 0, sizeof(uint8_t) * destlen);
+	if (reminder) {
+		dest[destlen - quotient - 1] = (src[0] >> 4);
+		for (int i = 0; i < quotient; i++) {
+			dest[destlen - quotient + i] = (src[i] << 4) | (src[i + 1] >> 4);
+		}
+	}
+	else {
+		memcpy(&dest[destlen - quotient], &src[0], sizeof(uint8_t) * quotient);
+	}
+
 
 }
-int makeRainbowTable(int N, const char* outputPath) {
+int makeRainbowTable(const char* outputPath) {
 	FILE* fo;
 	if (!(fo = fopen(outputPath, "wb"))) {
 		printf("Error: Output file open error\n");
 		exit(1);
 	}
-	const int SPACEBOUND = integerSqrt(1 << N);
+	const int SPACEBOUND = integerSqrt(1 << PWBITS);
 	struct AES_ctx ctx;
 	uint8_t plain[16];         //128 bit
-	uint8_t key[16];           //128 bit
+	uint8_t key1[16];          //128 bit
+	uint8_t key2[16];          //128 bit
 
 	memset(plain, 0, sizeof(plain));
-	memset(key, 0, sizeof(key));
+	memset(key1, 0, sizeof(key1));
 
-	AES_init_ctx(&ctx, key);
-	AES_ECB_encrypt(&ctx, plain);
-	for (int i = 0; i < 16; i++) {
-		printf("[%2x] ", plain[i]);
+
+	/*for (int i = 0; i < 16; i++) {
+		printf("%02x", plain[i]);
 	}
-	putchar('\n');
+	putchar('\n');*/
 
-	for (int i = 0; i < SPACEBOUND; i++) {
+	// for (int i = 0; i < SPACEBOUND; i++) {
+	for (int i = 0; i < 2; i++) {
+		fwrite(key1, 1, sizeof(key1), fo);
+		memcpy(key2, key1, sizeof(key2));
+		for (int j = 0; j < REDUCTCHAIN; j++) {
+			AES_init_ctx(&ctx, key2);
+			AES_ECB_encrypt(&ctx, plain);
+			// In this time plain is the Hash result.
+			printf("In this time, plain:\n");
+			for (int i = 0; i < 16; i++) {
+				printf("%02x", plain[i]);
+			}
+			putchar('\n');
 
+			reduction(key2, plain, 16, 16);
+			memset(plain, 0, sizeof(plain));
+
+			printf("Yes, now my key:\n");
+			for (int i = 0; i < 16; i++) {
+				printf("%02x", key2[i]);
+			}
+			putchar('\n');
+		}
 	}
 
 
@@ -78,23 +120,19 @@ int makeRainbowTable(int N, const char* outputPath) {
 	fclose(fo);
 }
 int main(int argc, char* argv[]) {
-	int N;
-	for (int i = 0; i <= 128; i++) {
-		printf("%d: %d\n", i, integerSqrt(i));
-	}
-	if (argc != 2) {
+	/*if (argc != 2) {
 		printf("Usage: Enter one command line argument. (non-negative integer)\n");
 		exit(1);
 	}
-	if (argv_valid_check(argv[1]) || (N = str_to_int(argv[1])) <= 0) {
+	if (argv_valid_check(argv[1]) || (PWBITS = str_to_int(argv[1])) <= 0) {
 		printf("Usage: Please enter a non-negative integer as argument\n");
 		exit(1);
 	}
-	if (N > 32 || N % 4) {
+	if (PWBITS > 32 || PWBITS % 4) {
 		printf("Usage: the argument is wrong ( <= 32 %% multiple of 4) \n");
 		exit(1);
-	}
-	N = 20;
-	makeRainbowTable(N, "rainbow");
+	}*/
+	PWBITS = 20;
+	makeRainbowTable("rainbow");
 
 }
