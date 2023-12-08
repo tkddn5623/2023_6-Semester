164c0cbec810a64ea048b70d9d542c7373d292bc
tkddn5623 <tkddn5623@naver.com>
2023-12-08 10:42:03 +0000

    mininet project begin...

---
+++ networkAssignment/test.py
@@ -0,0 +1,39 @@
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import OVSKernelSwitch    
+from mininet.cli import CLI
+from mininet.node import Host
+from mininet.log import setLogLevel, info
+from mininet.link import Link, TCLink
+import socket
+import time
+
+
+class MyTopology(Topo):
+    def build(self):
+        s0 = self.addSwitch("s0", cls=OVSKernelSwitch, failMode="standalone")
+        for i in range(10):
+            h = self.addHost(f"h{i}", cls=Host, defaultRoute=None)
+            self.addLink(h, s0, cls=TCLink, bw=10000000, delay="0.1ms", loss=0.001)
+
+def main():
+    topo = MyTopology()
+    net = Mininet(topo=topo, autoSetMacs=True, build=False, ipBase="10.0.0.0/24")
+
+#    net.build()
+    net.start()
+
+    for i in range(10):
+        h = net.getNodeByName(f"h{i}")
+        h.setIP(intf=f"h{i}-eth0", ip=f"10.0.0.{i + 1}/24")
+
+    src, dst = net.hosts[0], net.hosts[1]
+    s_bw, c_bw = net.iperf([src, dst], seconds=1)
+    s_bw, c_bw = net.iperf([net.hosts[2], dst], seconds=1)
+    info(s_bw)
+    # CLI(net)
+    net.stop()
+
+if __name__ == "__main__":
+    setLogLevel("info")
+    main()
--- networkAssignment/udp_server.py
+++ networkAssignment/udp_server.py
@@ -2,7 +2,7 @@ import socket
 
 def udp_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('10.0.0.2', 12345))
+    s.bind(('10.0.0.2', 1234))
     data, addr = s.recvfrom(1024)
     s.sendto("Hi I'm server!".encode('utf-8'), addr)
     s.close()
