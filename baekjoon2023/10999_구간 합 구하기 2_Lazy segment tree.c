ddf69f184206ffb6f8ea20014f0cc61a9bd09a97
tkddn5623 <tkddn5623@g.skku.edu>
2024-02-04 03:21:50 +0900

    Lazy propagation implementation

---
+++ baekjoon2023/10999_구간 합 구하기 2_Lazy segment tree.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXTREE (1 << 21)
+
+typedef struct {
+    long long item;
+    long long lazy;
+} SegmentTree;
+
+void _SG_propagate(SegmentTree segtree[], const int NI, const int NL, const int NR) {
+    if (segtree[NI].lazy == 0) return;
+    if (NL < NR) {
+        segtree[NI * 2].lazy += segtree[NI].lazy;
+        segtree[NI * 2 + 1].lazy += segtree[NI].lazy;
+    }
+    segtree[NI].item += segtree[NI].lazy * (NR - NL + 1);
+    segtree[NI].lazy = 0;
+}
+void _SG_update_impl(SegmentTree segtree[], const int from, const int to, const long long value, const int NI, const int NL, const int NR) {
+    _SG_propagate(segtree, NI, NL, NR);
+    if (from > NR || to < NL) return;
+    if (from <= NL && to >= NR) {
+        segtree[NI].lazy += value;
+        _SG_propagate(segtree, NI, NL, NR);
+        return;
+    }
+    const int mid = NL + (NR - NL) / 2;
+    _SG_update_impl(segtree, from, to, value, NI * 2, NL, mid);
+    _SG_update_impl(segtree, from, to, value, NI * 2 + 1, mid + 1, NR);
+    segtree[NI].item = segtree[NI * 2].item + segtree[NI * 2 + 1].item;
+}
+long long _SG_query_impl(SegmentTree segtree[], const int from, const int to, const int NI, const int NL, const int NR) {
+    _SG_propagate(segtree, NI, NL, NR);
+    if (from > NR || to < NL) return 0LL;
+    if (from <= NL && to >= NR) return segtree[NI].item;
+    const int mid = NL + (NR - NL) / 2;
+    return _SG_query_impl(segtree, from, to, NI * 2, NL, mid) + _SG_query_impl(segtree, from, to, NI * 2 + 1, mid + 1, NR);
+}
+void SG_construct(SegmentTree _Dest[], const long long items[], const int item_len) {
+    int leafs = 1;
+    while (leafs < item_len) { leafs <<= 1; }
+    for (int i = 0; i < item_len; i++) {
+        _Dest[i + leafs].item = items[i];
+        _Dest[i + leafs].lazy = 0LL;
+    }
+    for (int i = leafs - 1; i >= 1; i--) {
+        _Dest[i].item = _Dest[i * 2].item + _Dest[i * 2 + 1].item;
+        _Dest[i].lazy = 0LL;
+    }
+    _Dest[0].item = leafs;
+}
+void SG_update(SegmentTree segtree[], const int from, const int to, const long long value) {
+    _SG_update_impl(segtree, from, to, value, 1, 0, segtree[0].item - 1);
+}
+long long SG_query(SegmentTree segtree[], const int from, const int to) {
+    return _SG_query_impl(segtree, from, to, 1, 0, segtree[0].item - 1);
+}
+int main() {
+    //freopen("i.txt", "r", stdin);
+    static SegmentTree segtree[MAXTREE];
+    long long* arr;
+    int N, M, K;
+
+    scanf("%d%d%d", &N, &M, &K);
+    if (!(arr = calloc(N, sizeof(long long)))) exit(1);
+
+    for (int i = 0; i < N; i++) { scanf("%lld", &arr[i]); }
+    SG_construct(segtree, arr, N);
+    free(arr);
+
+    for (int i = 0; i < M + K; i++) {
+        int type;
+        long long a, b, c;
+
+        scanf("%d", &type);
+
+        if (type == 1) {
+            scanf("%lld%lld%lld", &a, &b, &c);
+            SG_update(segtree, a - 1, b - 1, c);
+        }
+        else if (type == 2) {
+            scanf("%lld%lld", &a, &b);
+            printf("%lld\n", SG_query(segtree, a - 1, b - 1));
+        }
+    }
+}
--- myCLibrary/SegmentTree_Basic.c
+++ myCLibrary/SegmentTree_Basic.c
@@ -1,4 +1,5 @@
 #define MAXTREE (1 << 21)
+
 void SG_construct(long long _Dest[], const long long items[], const int item_len) {
 	int leafs = 1;
 	while (leafs < item_len) { leafs <<= 1; }
@@ -10,7 +11,14 @@ void SG_construct(long long _Dest[], const long long items[], const int item_len
 	}
 	_Dest[0] = leafs;
 }
-long long SG_get(const long long segtree[], int from, int to) {
+void SG_update(long long segtree[], int index, const long long value) {
+	index += segtree[0];
+	segtree[index] = value;
+	while ((index /= 2) >= 1) {
+		segtree[index] = segtree[index * 2] + segtree[index * 2 + 1];
+	}
+}
+long long SG_query(const long long segtree[], int from, int to) {
 	long long ret = 0;
 	from += segtree[0]; to += segtree[0];
 	while (from <= to) {
@@ -20,22 +28,15 @@ long long SG_get(const long long segtree[], int from, int to) {
 	}
 	return ret;
 }
-void SG_update(long long segtree[], int index, const long long value) {
-	index += segtree[0];
-	segtree[index] = value;
-	while ((index /= 2) >= 1) {
-		segtree[index] = segtree[index * 2] + segtree[index * 2 + 1];
-	}
-}
 
-long long _SG_get_topdown_impl(const long long segtree[], const int from, const int to, const int NI, const int NL, const int NR) {
+long long _SG_query_topdown_impl(const long long segtree[], const int from, const int to, const int NI, const int NL, const int NR) {
 	if (from > NR || to < NL) return 0;
 	if (from <= NL && to >= NR) return segtree[NI];
 	const int mid = NL + (NR - NL) / 2;
-	return _SG_get_topdown_impl(segtree, from, to, NI * 2, NL, mid) + _SG_get_topdown_impl(segtree, from, to, NI * 2 + 1, mid + 1, NR);
+	return _SG_query_topdown_impl(segtree, from, to, NI * 2, NL, mid) + _SG_query_topdown_impl(segtree, from, to, NI * 2 + 1, mid + 1, NR);
 }
-long long SG_get_topdown(const long long segtree[], const int from, const int to) {
-	return _SG_get_topdown_impl(segtree, from, to, 1, 0, segtree[0] - 1);
+long long SG_query_topdown(const long long segtree[], const int from, const int to) {
+	return _SG_query_topdown_impl(segtree, from, to, 1, 0, segtree[0] - 1);
 }
 
 
