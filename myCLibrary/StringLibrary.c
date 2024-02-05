0b420b6aab6f3172a4849663ea40be5fe75a5fb9
tkddn5623 <tkddn5623@g.skku.edu>
2024-02-05 18:28:39 +0900

    String lib added (just for ps, simple functions)

---
+++ myCLibrary/StringLibrary.c
@@ -0,0 +1,37 @@
+void stdin_getline(char* dest) {
+	for (*dest = 0, fgets(dest, BUFMAX, stdin); *dest & ~0x1f; dest++);; *dest = 0;
+}
+int string_tokenizer(char* tokens[], char* str) {
+	int ch, i = 0;
+	do {
+		while ((ch = *str) == ' ') *str++ = '\0';
+		if (ch) tokens[i++] = str++;
+		while ((ch = *str) != ' ' && ch) str++;
+	} while (ch);
+	return i;
+}
+int string_compare(const char* a, const char* b) {
+	while (*a && *a == *b) a++, b++;
+	return *a - *b;
+}
+void string_copy(char* a, const char* b) {
+	while (*a = *b++);
+}
+
+
+/*
+// Tokenizer <string.h> version
+
+#define MAXTOK (999)
+	char* tokens[MAXTOK];
+	int tokens_size;
+	tokens[(tokens_size = 0)] = strtok(buf, " ");
+	while (++tokens_size < MAXTOK && (tokens[tokens_size] = strtok(NULL, " ")));
+
+*/
+
+/*
+* 2024.2.5 Mon
+* <string.h> imitation
+* Reference of {compare, copy}: bloodstrawberry.tistory.com
+*/
